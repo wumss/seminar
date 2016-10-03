53c7ea3ce464200df235afb0b65ca81ccd178507
Fengyang Wang <fengyang.wang.0@gmail.com>
2016-10-02 22:35:15 -0400

    Configure MathJax properly

--- rebuild_schedule.jl
+++ rebuild_schedule.jl
@@ -13,6 +13,19 @@ title = "Upcoming Talks"
 type = "home-section"
 +++
 
+<script type="text/x-mathjax-config">
+  MathJax.Hub.Config({
+    extensions: ["tex2jax.js"],
+    jax: ["input/TeX", "output/HTML-CSS"],
+    tex2jax: {
+      inlineMath: [ ['$','$'], ["\\(","\\)"] ],
+      displayMath: [ ['$$','$$'], ["\\[","\\]"] ],
+      processEscapes: true
+    },
+    "HTML-CSS": { availableFonts: ["TeX"] }
+  });
+</script>
+
 <script type="text/javascript"
   src="https://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS-MML_CHTML">
 </script>
