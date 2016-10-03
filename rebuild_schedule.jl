154973b19a4542210d265a90cbe3b90660370f7b
Fengyang Wang <fengyang.wang.0@gmail.com>
2016-10-02 22:38:12 -0400

    Escape $ signs

--- rebuild_schedule.jl
+++ rebuild_schedule.jl
@@ -18,8 +18,8 @@ type = "home-section"
     extensions: ["tex2jax.js"],
     jax: ["input/TeX", "output/HTML-CSS"],
     tex2jax: {
-      inlineMath: [ ['$','$'], ["\\(","\\)"] ],
-      displayMath: [ ['$$','$$'], ["\\[","\\]"] ],
+      inlineMath: [ ['\$','\$'], ["\\(","\\)"] ],
+      displayMath: [ ['\$\$','\$\$'], ["\\[","\\]"] ],
       processEscapes: true
     },
     "HTML-CSS": { availableFonts: ["TeX"] }
