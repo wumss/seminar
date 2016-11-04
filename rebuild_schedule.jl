9e8670cfba1bb669e14d5c00bf5dfa9286a5bb4d
Fengyang Wang <fengyang.wang.0@gmail.com>
2016-11-04 01:05:57 -0400

    Replace old home page with new home page

--- rebuild_schedule.jl
+++ rebuild_schedule.jl
@@ -1,7 +1,6 @@
 #!/usr/bin/env julia
 
 using JSON
-using TextWrap  # English once its wrapping capability is finished
 
 include("build_help.jl")
 
@@ -16,58 +15,12 @@ identifier(t) = string(
     String(collect(take(filter(x -> !isspace(x), lowercase(t[:topic])), 10))),
     hash(t) % UInt16)
 
-function render_markdown_from_file(io, t, sym, indent)
-    abstract_md = readstring(joinpath(string(sym), t[sym]))
-    abstract_html = stringmime("text/html",
-                               Base.Markdown.parse(abstract_md))
-    println_wrapped(io,
-        abstract_html,
-        width=79,
-        initial_indent=indent,
-        subsequent_indent=indent,
-        replace_whitespace=true,
-        break_long_words=false
-    )
-end
-
-function print_row(t)
-    println("""
-            <tr>
-              <td>$(t[:topic])</td>
-              <td>$(t[:speaker])</td>
-              <td>$(t[:location])</td>
-              <td>$(String(collect(take(t[:time], 5))))</td>
-            </tr>""")
-    if haskey(t, :abstract)
-        println("<tr>")
-        println("  <td colspan=4>")
-        render_markdown_from_file(STDOUT, t, :abstract, 4)
-        println("  </td>")
-        println("</tr>")
-    end
-end
-
 function write_summary(t)
     generate_page(merge(Dict(
         :title => t[:topic],
         :pagetype => "archive"
     ), t), "archive/$(identifier(t))")
 end
-
-println("""
-+++
-date = "2016-09-23T21:24:42-04:00"
-title = "Upcoming Talks"
-type = "home-section"
-+++
-
-""")
-
-println("""
-## Upcoming Talks
-
-See the [archive](archive) for talks before today’s date.
-""")
 
 result = JSON.parsefile("schedule.json", dicttype=Dict{Symbol,String})
 sort(result, by=x -> x[:time])
@@ -76,17 +29,6 @@ map!(result) do d
     d
 end
 dates = unique(map(x -> x[:date], result))
-println("""
-        <table>
-        <thead>
-        <tr>
-          <th>Topic</th>
-          <th>Speaker</th>
-          <th>Location</th>
-          <th>Time</th>
-        </tr>
-        </thead>
-        <tbody>""")
 
 summarize(t) = "Talk by $(t[:speaker])."
 
@@ -100,23 +42,18 @@ for d in dates
                 :url => "/seminar/archive/$(identifier(t))",
                 :summary => summarize(t)))
         end
-    else
-        println("<tr><th colspan=4>Talks on $(human(d))</th></tr>")
-        for t in filter(x -> x[:date] == d, result)
-            print_row(t)
-        end
     end
 end
-println("</tbody>")
-println("</table>")
 
 generate_page(Dict(
     :title => "Archived Talks",
     :pagetype => "archived-talks",
-    :talks => talks), "archive")
+    :talks => talks,
+    :mathjaxplease => true), "archive")
 
 generate_page(Dict(
     :title => "Mathematics Student Seminars",
     :pagetype => "home",
     :dates => dates,
-    :talks => result), "home")
+    :talks => result,
+    :mathjaxplease => true), "")
