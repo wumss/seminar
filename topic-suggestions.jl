c599af08b7889a208c72168dc4b8ae23581d73a6
Fengyang Wang <fengyang.wang.0@gmail.com>
2016-11-05 23:53:36 -0400

    Sort suggestions alphabetically

--- topic-suggestions.jl
+++ topic-suggestions.jl
@@ -1,5 +1,6 @@
 suggestions = JSON.parsefile("data/suggested-topics.json";
                              dicttype=Dict{Symbol,Any})
+sort!(suggestions, by=x -> x[:topic])
 
 bytag = DefaultDict(String, Vector{Any}, () -> [])
 for s in suggestions
