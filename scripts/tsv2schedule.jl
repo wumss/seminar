3dabae225e91c6ae8139181833fe68ec4b46d42c
Fengyang Wang <fengyang.wang.0@gmail.com>
2017-01-08 21:29:36 -0500

    fix script and schedule

--- scripts/tsv2schedule.jl
+++ scripts/tsv2schedule.jl
@@ -1,14 +1,14 @@
 using JSON
 data = readdlm("input.tsv", '\t')
 talks = []
-for row in 1:size(data, 2)
+for row in 1:size(data, 1)
     if length(data[row,1]) == 10
         for (i, j, k) in ((2, 3, 17), (4, 5, 18))
             push!(talks, Dict(
                 "speaker"    => data[row,i],
                 "topic"      => data[row,j],
                 "location"   => "MC 4045",
-                "time"       => "$(row[1])T$k:30:00Z",
+                "time"       => "$(data[row,1])T$k:30:00Z",
                 "tags"       => [],
                 "identifier" => "$(data[row,i])-change-me"
             ))
