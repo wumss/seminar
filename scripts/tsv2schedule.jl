a39ac9cb62a916e7fde49474ac5775eea2882aaa
Fengyang Wang <fengyang.wang.0@gmail.com>
2017-01-08 21:21:12 -0500

    New schedule

---
+++ scripts/tsv2schedule.jl
@@ -0,0 +1,18 @@
+using JSON
+data = readdlm("input.tsv", '\t')
+talks = []
+for row in 1:size(data, 2)
+    if length(data[row,1]) == 10
+        for (i, j, k) in ((2, 3, 17), (4, 5, 18))
+            push!(talks, Dict(
+                "speaker"    => data[row,i],
+                "topic"      => data[row,j],
+                "location"   => "MC 4045",
+                "time"       => "$(row[1])T$k:30:00Z",
+                "tags"       => [],
+                "identifier" => "$(data[row,i])-change-me"
+            ))
+        end
+    end
+end
+println(JSON.json(talks, 4))
