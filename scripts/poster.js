116c108b60622c82e9b247c5ce1363381b617b5d
Frieda Rong <adeirff@gmail.com>
2017-01-25 00:01:46 +0000

    Built from commit b8b6e3d457042860de8fdd081f8ccc486e429c89

--- scripts/poster.js
+++ scripts/poster.js
@@ -1,5 +1,5 @@
 var colorPick = document.getElementById('color-pick');
-var miniPage = document.getElementById('minipage');
+var miniPage = document.querySelector('.minipage');
 
 colorPick.onchange = function(e) {
     miniPage.style.backgroundColor = colorPick.value;
