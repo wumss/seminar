696e258b907198177c3a0dd7cc66bf239be36de0
Fengyang Wang <fengyang.wang.0@gmail.com>
2017-02-19 15:20:27 -0500

    pttool enhancement

--- static/scripts/pttool.js
+++ static/scripts/pttool.js
@@ -1,7 +1,14 @@
+new Clipboard('.copyclipboard');
+
 const URL = 'https://raw.githubusercontent.com/wumss/seminar/master/data/suggested-topics.json';
 const resultBox = document.querySelector('#committed');
 const statusBox = document.querySelector('#status');
 const topicList = document.querySelector('#suggested');
+
+const addTopicTopic = document.querySelector('#attopic');
+const addTopicTags = document.querySelector('#attags');
+const addTopicReferences = document.querySelector('#atreferences');
+const addTopicExcerpt = document.querySelector('#atexcerpt');
 
 let baseData = null;
 const delta = [];
@@ -11,7 +18,14 @@ const logStatus = function (err) {
 };
 
 const computePresentData = function () {
-    return baseData;
+    const result = [];
+    for (let obj of baseData) {
+        result.push(obj);
+    }
+    for (let obj of delta) {
+        result.push(obj);
+    }
+    return result;
 };
 
 const canonicalize = function (json) {
@@ -30,6 +44,34 @@ const updateResultToCommit = function () {
         topicList.appendChild(ele);
     }
 };
+
+const splitOn = function (str, chr) {
+    return str.split(chr).map(x => x.trim()).filter(x => x);
+};
+
+const updateTopicToBeAdded = function () {
+    if (!delta.length) {
+        delta.push({});
+    }
+    const thisdelta = delta[0];
+    thisdelta.topic = addTopicTopic.value;
+    thisdelta.tags = splitOn(addTopicTags.value, ',');
+    thisdelta.references = splitOn(addTopicReferences.value, '\n');
+    const excerpt = addTopicExcerpt.value.trim();
+    if (excerpt) {
+        thisdelta.excerpt = excerpt;
+    }
+    updateResultToCommit();
+};
+
+const defer = function (func) {
+    return () => window.setTimeout(func, 0);
+};
+
+addTopicTopic.onkeypress = defer(updateTopicToBeAdded);
+addTopicTags.onkeypress = defer(updateTopicToBeAdded);
+addTopicReferences.onkeypress = defer(updateTopicToBeAdded);
+addTopicExcerpt.onkeypress = defer(updateTopicToBeAdded);
 
 fetch(URL).then(
     response => response.json()
