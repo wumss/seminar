834c4e06b69814c48ae6c0907058b817cdd6b516
Frieda Rong <adeirff@gmail.com>
2017-09-04 06:06:50 +0000

    Built from commit 2ef5c7f7e4ab7bc501a135070ce0064061d54dca

--- scripts/pttool.js
+++ scripts/pttool.js
@@ -1,84 +1,140 @@
-new Clipboard('.copyclipboard');
+new Clipboard('.copyclipboard')
 
-const URL = 'https://raw.githubusercontent.com/wumss/seminar/master/data/suggested-topics.json';
-const resultBox = document.querySelector('#committed');
-const statusBox = document.querySelector('#status');
-const topicList = document.querySelector('#suggested');
+const SchemaURL = '/scripts/schema/topic.json'
+const URL = 'https://raw.githubusercontent.com/wumss/seminar/master/data/suggested-topics.json'
+const resultBox = document.querySelector('#committed')
+const statusBox = document.querySelector('#status')
+const topicList = document.querySelector('#suggested')
 
-const addTopicTopic = document.querySelector('#attopic');
-const addTopicTags = document.querySelector('#attags');
-const addTopicReferences = document.querySelector('#atreferences');
-const addTopicExcerpt = document.querySelector('#atexcerpt');
+let editor = null
+let baseData = null
+const delta = []
 
-let baseData = null;
-const delta = [];
+const dependencyStatus = {
+  json: false,
+  schema: false
+}
+const phases = ['wait', 'select', 'edit']
+let currentPhase = 0
+
+const moveToNextPhase = function () {
+  currentPhase += 1
+  updatePhase()
+}
+
+const updatePhase = function () {
+  phases.forEach(phase => {
+    if (phase !== phases[currentPhase]) {
+      document.getElementById(`phase-${phase}`).style.display = 'none'
+    } else {
+      document.getElementById(`phase-${phase}`).style.display = 'block'
+    }
+  })
+  if (phases[currentPhase] === 'edit') {
+    delta.push({
+      action: 'create',
+      body: {}
+    })
+    updateTopicToBeAdded()
+  }
+}
+
+const registerAsLoaded = function (type) {
+  dependencyStatus[type] = true
+  logLoadedStatus()
+}
 
 const logStatus = function (err) {
-    statusBox.textContent = err;
-};
+  statusBox.textContent = err
+}
+
+const logLoadedStatus = function () {
+  if (dependencyStatus.json && dependencyStatus.schema) {
+    logStatus('All data loaded; you may proceed.')
+    moveToNextPhase()
+  } else if (dependencyStatus.json) {
+    logStatus('Please wait for schema to load.')
+  } else if (dependencyStatus.schema) {
+    logStatus('Please wait for JSON to load.')
+  } else {
+    logStatus('Please wait for data to load.')
+  }
+}
 
 const computePresentData = function () {
-    const result = [];
-    for (let obj of baseData) {
-        result.push(obj);
+  let result = []
+  for (let obj of baseData) {
+    result.push(obj)
+  }
+  for (let obj of delta) {
+    if (obj.action === 'delete') {
+      result = result.filter(o => o.topic !== obj.topic)
+    } else if (obj.action === 'create') {
+      result.push(obj.body)
     }
-    for (let obj of delta) {
-        result.push(obj);
-    }
-    return result;
-};
+  }
+  return result
+}
 
 const canonicalize = function (json) {
-    return json.sort((f, g) => f.topic.localeCompare(g.topic));
-};
+  return json.sort((f, g) => f.topic.localeCompare(g.topic))
+}
 
 const updateResultToCommit = function () {
-    let json = canonicalize(computePresentData());
-    resultBox.value = JSON.stringify(json, null, 4);
-    while (topicList.firstChild) {
-        topicList.removeChild(topicList.firstChild);
-    }
-    for (let object of json) {
-        const ele = document.createElement('li');
-        ele.textContent = object.topic;
-        topicList.appendChild(ele);
-    }
-};
-
-const splitOn = function (str, chr) {
-    return str.split(chr).map(x => x.trim()).filter(x => x);
-};
+  let json = canonicalize(computePresentData())
+  resultBox.value = JSON.stringify(json, null, 4)
+  while (topicList.firstChild) {
+    topicList.removeChild(topicList.firstChild)
+  }
+  for (let object of json) {
+    const ele = document.createElement('li')
+    const anchor = document.createElement('a')
+    anchor.addEventListener('click', function () {
+      delta.push({
+        action: 'delete',
+        topic: object.topic
+      })
+      editor.setValue(object)
+      moveToNextPhase()
+    })
+    ele.appendChild(anchor)
+    anchor.textContent = object.topic
+    topicList.appendChild(ele)
+  }
+}
 
 const updateTopicToBeAdded = function () {
-    if (!delta.length) {
-        delta.push({});
-    }
-    const thisdelta = delta[0];
-    thisdelta.topic = addTopicTopic.value;
-    thisdelta.tags = splitOn(addTopicTags.value, ',');
-    thisdelta.references = splitOn(addTopicReferences.value, '\n');
-    const excerpt = addTopicExcerpt.value.trim();
-    if (excerpt) {
-        thisdelta.excerpt = excerpt;
-    }
-    updateResultToCommit();
-};
+  let idx = delta.length - 1
+  delta[idx].body = editor.getValue()
+  updateResultToCommit()
+}
 
 const defer = function (func) {
-    return () => window.setTimeout(func, 0);
-};
+  return () => window.setTimeout(func, 0)
+}
 
-addTopicTopic.onkeypress = defer(updateTopicToBeAdded);
-addTopicTags.onkeypress = defer(updateTopicToBeAdded);
-addTopicReferences.onkeypress = defer(updateTopicToBeAdded);
-addTopicExcerpt.onkeypress = defer(updateTopicToBeAdded);
+document.querySelector('#new-topic').addEventListener('click', function () {
+  moveToNextPhase()
+})
+
+fetch(SchemaURL).then(
+  response => response.json()
+).then(function (json) {
+  // Initialize the editor
+  editor = new JSONEditor(document.getElementById('editor-container'), {
+    schema: json
+  })
+
+  editor.on('change', () => defer(updateTopicToBeAdded))
+  registerAsLoaded('schema')
+})
 
 fetch(URL).then(
-    response => response.json()
+  response => response.json()
 ).then(function (json) {
-    baseData = json;
-    updateResultToCommit();
-    logStatus('Successfully loaded data.');
+  baseData = json
+  updateResultToCommit()
+  registerAsLoaded('json')
 }).catch(function (ex) {
-    logStatus(`Loading or parsing of JSON failed: ${ex}`);
-});
+  logStatus(`Loading or parsing of JSON failed: ${ex}`)
+})
