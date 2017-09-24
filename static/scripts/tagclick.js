81c1a5000412d72b48c7e7c6fe4d54c833b2c0a0
Fengyang Wang <fengyang.wang.0@gmail.com>
2017-09-24 17:37:45 -0400

    reenable tag finder

--- static/scripts/tagclick.js
+++ static/scripts/tagclick.js
@@ -1,3 +1,9 @@
-document.querySelector('#tagclick').onclick = function() {
-    location.href = '/tag/' + document.querySelector('#selectedtag').value;
+document.querySelector('#tagclick').onclick = function () {
+  const tagItems = document.querySelectorAll('.tag-link')
+  const wantedTag = document.querySelector('#selectedtag').value
+  for (let tagLink of tagItems) {
+    if (tagLink.textContent === wantedTag) {
+      window.location.href = tagLink.href
+    }
+  }
 }
